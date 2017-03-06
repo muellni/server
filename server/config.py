98d051c9685af1fcc55bcac642dc9cc30faf2d10
Martin Müllenhaupt <mm+git@netlair.de>
2017-03-06 10:28:33 +0100

    #271 change Twilio auth to use API key

--- server/config.py
+++ server/config.py
@@ -67,8 +67,9 @@ API_BASE_URL = os.getenv("API_BASE_URL", "https://api.dev.faforever.com/jwt")
 
 FAF_POLICY_SERVER_BASE_URL = os.getenv("FAF_POLICY_SERVER_BASE_URL", "http://faf-policy-server")
 
-TWILIO_SID = os.getenv("TWILIO_SID", "")
-TWILIO_KEY = os.getenv("TWILIO_KEY", "")
+TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID", "")
+TWILIO_API_KEY = os.getenv("TWILIO_API_KEY", "")
+TWILIO_API_SECRET = os.getenv("TWILIO_API_SECRET", "")
 TWILIO_TTL = os.getenv("TWILIO_TTL", None)
 
 COTURN_HOSTS = os.getenv('COTURN_HOSTS', "").split(',')
