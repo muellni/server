c368eb94a49953082fc2c69c7aeddb7ed84973d9
Martin Müllenhaupt <mm+git@netlair.de>
2017-06-09 10:52:26 +0200

    fix hard-coded encoded key size

--- server/config.py
+++ server/config.py
@@ -42,13 +42,10 @@ PRIVATE_KEY_BLOBS = os.getenv("FAF_PRIVATE_KEY", '').split(';')
 print('CONFIG: {} private key blobs found in env'.format(len(PRIVATE_KEY_BLOBS)), file=sys.stderr)
 for KEYBLOB in PRIVATE_KEY_BLOBS:
     try:
-        PRIVATE_KEY = rsa.PrivateKey.load_pkcs1(base64.b64decode(KEYBLOB), format='DER')
-        keybits = PRIVATE_KEY.n.bit_length() + 7 - ((PRIVATE_KEY.n.bit_length() + 7)%8) # round up to multiple of 8 (byte)
-        _aes_key_base64_size = 4*keybits/24
-        _aes_key_base64_size = int(_aes_key_base64_size + 3 - ((_aes_key_base64_size + 3)%4)) # round to multiple of 4
-        PRIVATE_KEYS.append(PRIVATE_KEY)
+        PRIVATE_KEYS.append(rsa.PrivateKey.load_pkcs1(base64.b64decode(KEYBLOB), format='DER'))
+        _aes_key_base64_size = 4*PRIVATE_KEY.n.bit_length()/24
+        _aes_key_base64_size = _aes_key_base64_size + 3 - ((_aes_key_base64_size + 3)%4) # round to multiple of 4
         AES_KEY_BASE64_SIZES.append(_aes_key_base64_size)
-        print('CONFIG: Loaded {}bit rsa key, aes key size {}'.format(keybits, _aes_key_base64_size), file=sys.stderr)
     except:
         print(traceback.format_exc(), file=sys.stderr)
 print('CONFIG: {} private keys loaded'.format(len(PRIVATE_KEYS)), file=sys.stderr)
